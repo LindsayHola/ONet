46bed1274e1a7eb360c60c1007ced4430981287f
OholaLindsay <lindsayhola@gmail.com>
2025-09-20 15:45:48 +0800

    added atomic int

---
+++ src/main/java/ONet/AtomicInt.java
@@ -0,0 +1,29 @@
+packsage src.main.java.ONet
+import org.junit.jupiter.api.Test;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class AtomicInt {
+        private static int THREAD_COUNT = 10;
+        @Test
+        public static void main(String[] args) throws InterruptedException
+        {
+            CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
+            AtomicInteger atomicInteger = new AtomicInteger(0);
+            for (int i = 0; i < THREAD_COUNT; i++)
+            {
+                ThreadUtil.getMixedTargetThreadPool().submit(() ->
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        atomicInteger.getAndIncrement();
+                    }
+                    latch.countDown();
+                });
+            }
+            latch.await();
+            System.out.println("sum=" + atomicInteger.get());
+        }
+
+}
